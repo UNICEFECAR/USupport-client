dec8e8efc3b805bca4519f772c4d09609460101c
Georgi 7DIGIT <georgi@7digit.io>
2023-05-13 02:49:16 +0300

    Refactor: remove references to the "phonePrefix" column

--- service/queries/providers.js
+++ service/queries/providers.js
@@ -3,7 +3,7 @@ import { getDBPool } from "#utils/dbConfig";
 export const getProviderByIdQuery = async ({ poolCountry, providerId }) =>
   await getDBPool("piiDb", poolCountry).query(
     `
-      SELECT provider_detail."provider_detail_id", "name", patronym, surname, nickname, email, phone_prefix, phone, image, specializations, street, city, postcode, education, sex, consultation_price, description, video_link
+      SELECT provider_detail."provider_detail_id", "name", patronym, surname, nickname, email, phone, image, specializations, street, city, postcode, education, sex, consultation_price, description, video_link
       FROM provider_detail
         JOIN "user" ON "user".provider_detail_id = provider_detail.provider_detail_id AND "user".deleted_at IS NULL
       WHERE provider_detail.provider_detail_id = $1
