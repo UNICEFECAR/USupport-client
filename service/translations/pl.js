a8f35b0ab12e95753839f3330118169f85738828
Sebastian Galustyan <sebastian@7digit.io>
2024-05-20 13:17:34 +0100

    Add: new translations uk and pl

---
+++ service/translations/pl.js
@@ -0,0 +1,16 @@
+export default {
+  email_required_error: "Należy podać ważny adres e-mail",
+  access_token_required_error: "Należy podać prawidłowy token dostępu",
+  nickname_required_error: "Musisz podać nick",
+  client_not_found_error: "Nie znaleziono klienta",
+  provider_not_found_error: "Nie znaleziono konsultanta",
+  incorrect_password_error: "Podane hasło jest nieprawidłowe",
+  email_already_used_error: "Ten adres e-mail jest już wykorzystany",
+  coupon_not_found_error: "Nie znaleziono kuponu",
+  client_limit_reached_error:
+    "Osiągnąłeś maksymalną liczbę konsultacji objętych tą kampanią",
+  coupons_limit_reached_error:
+    "Osiągnięto maksymalną liczbę konsultacji objętych tą kampanią",
+  consultation_not_found_error: "Nie znaleziono konsultacji",
+  error_occured: "An error occurred PL",
+};
---
+++ service/translations/uk.js
@@ -0,0 +1,16 @@
+export default {
+  email_required_error: "Потрібно вказати дійсну електронну пошту",
+  access_token_required_error: "Потрібно надати дійсний токен доступу",
+  nickname_required_error: "Потрібно вказати нікнейм",
+  client_not_found_error: "Клієнта(ки) не знайдено",
+  provider_not_found_error: "Консультанта(ки) не знайдено",
+  incorrect_password_error: "Наданий пароль є невірним",
+  email_already_used_error: "Ця електронна адреса вже використовується",
+  coupon_not_found_error: "Купон не знайдено",
+  client_limit_reached_error:
+    "Ви досягли максимальної кількості консультацій, що покриваються цією кампанією",
+  coupons_limit_reached_error:
+    "Досягнуто максимальної кількості консультацій, охоплених цією кампанією",
+  consultation_not_found_error: "Консультацію не знайдено",
+  error_occured: "An error occurred UK",
+};
