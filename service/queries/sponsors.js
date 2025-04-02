608621b851f04556ee72187386491714c975d7c3
7Digit-Main <it-support@7digit.io>
2025-04-02 14:47:57 +0300

    Add: coupon_code field in the response of getAllConsultations

--- service/queries/sponsors.js
+++ service/queries/sponsors.js
@@ -6,7 +6,7 @@ export const getSponsorNameAndImageByCampaignIdQuery = async ({
 }) => {
   const campaigns = await getDBPool("piiDb", poolCountry).query(
     `
-        SELECT sponsor.name as sponsor_name, sponsor.image as sponsor_image, campaign.campaign_id
+        SELECT sponsor.name as sponsor_name, sponsor.image as sponsor_image, campaign.campaign_id, campaign.coupon_code
             FROM campaign
             JOIN sponsor ON sponsor.sponsor_id = campaign.sponsor_id
         WHERE campaign_id = ANY($1)
